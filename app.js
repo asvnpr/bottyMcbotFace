0eb1a12f2f36b8f860dbf32be32d9928da36d428
Alejandro <vega360@gmail.com>
2016-10-18 01:49:52 +0000

    added appID and password. really bad practice, but I'm just making this bot for practice. may delete this commit later anyway

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
 // Add your requirements
-var restify = require('restify'); 
-var builder = require('botbuilder'); 
+var restify = require('restify');
+var builder = require('botbuilder');
 
 // Setup Restify Server
 var server = restify.createServer();
-server.listen(process.env.PORT || 3000, function() 
+server.listen(process.env.PORT || 3000, function()
 {
-   console.log('%s listening to %s', server.name, server.url); 
+   console.log('%s listening to %s', server.name, server.url);
 });
 
 // Create chat bot
 var connector = new builder.ChatConnector
-({ appId: 'YourAppId', appPassword: 'YourAppPassword' }); 
+({ appId: 'b811d577-e934-4a6f-90e0-c150ad237bd0', appPassword: '7qd1oUpeD9k8Gt2YbWAeyWh' });
 var bot = new builder.UniversalBot(connector);
 server.post('/api/messages', connector.listen());
 
